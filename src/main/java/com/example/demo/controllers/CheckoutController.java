8a0d1f727bb136d911494f7f1e2e25fb4990aa8d
tashwo11 <tashwo11@wgu.edu>
2025-03-22 16:45:50 -0700

    #Task H Finished & Completed -Created CheckoutController for the controllers package

---
+++ src/main/java/com/example/demo/controllers/CheckoutController.java
@@ -0,0 +1,33 @@
+package com.example.demo.controllers;
+
+import com.example.demo.services.CheckoutService;
+import com.example.demo.services.Purchase;
+import com.example.demo.services.PurchaseResponse;
+import org.springframework.beans.factory.annotation.Autowired;
+
+import org.springframework.web.bind.annotation.*;
+
+@CrossOrigin("http://localhost:4200")
+@RestController
+@RequestMapping("/api/checkout")
+public class CheckoutController {
+
+    //CheckoutService instance for handling the purchase logic.
+    private CheckoutService checkoutService;
+
+    //Constructor to inject the CheckoutService dependency.
+    @Autowired
+    public CheckoutController(CheckoutService checkoutService) {
+        this.checkoutService = checkoutService;
+    }
+
+    //Handles POST requests to initiate a purchase order
+    @PostMapping("/purchase")
+    public PurchaseResponse placeOrder(@RequestBody Purchase purchase) {
+
+        //Process the purchase order and return the response with order tracking number
+        PurchaseResponse purchaseResponse = checkoutService.placeOrder(purchase);
+
+        return purchaseResponse;
+    }
+}
