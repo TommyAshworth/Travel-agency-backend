5393cd902e97536a76e24253843b2632166064ca
tashwo11 <tashwo11@wgu.edu>
2025-03-22 17:37:09 -0700

    #Bug -Fixed a bug, had to update dependencies changed pom.xml line 8 to version 3.3.2 to get order tracking number

--- src/main/java/com/example/demo/controllers/CheckoutController.java
+++ src/main/java/com/example/demo/controllers/CheckoutController.java
@@ -7,7 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 
 import org.springframework.web.bind.annotation.*;
 
-@CrossOrigin("http://localhost:4200")
+@CrossOrigin
 @RestController
 @RequestMapping("/api/checkout")
 public class CheckoutController {
